c86a198062cab859e18519263452b48a68da10cd
amhayslip <amhayslip@gmail.com>
2016-05-02 21:24:06 +0300

    remove items for start

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
-var jwt = require('express-jwt');
-var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
-
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var User = require('./UserModel');
-
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/passportjwt');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
+mongoose.connect('mongodb://localhost/passportjwt');
+
 var LocalStrategy = require('passport-local').Strategy;
-
-passport.use('login', new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-
-      return done(null, user);
-    });
-  }
-));
 
 app.get('/register', function (req, res) {
   res.sendFile(__dirname + '/register.html');
@@ -36,41 +19,20 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/hello', auth, function (req, res, next) {
+app.get('/hello', function (req, res) {
   res.json({hello: 'world'});
-});
-
-app.post('/register', function(req, res, next){
-  if(!req.body.username || !req.body.password){
-    return res.status(400).json({message: 'Please fill out all fields'});
-  }
-
-  var user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  user.save(function (err){
-    if(err){ return next(err); }
-
-    return res.json({token: user.generateJWT()})
-  });
 });
 
 app.get('/login', function (req, res) {
   res.sendFile(__dirname + '/login.html');
 });
 
-app.post('/login', function(req, res, next){
-  passport.authenticate('login', function(err, user){
-    if(err){ return next(err); }
+app.post('/register', function(req, res){
 
-    if (user) {
-      return res.json({token: user.generateJWT()});
-    } else {
-      return res.status(401);
-    }
-  })(req, res, next);
+});
+
+app.post('/login', function(req, res){
+
 });
 
 app.listen(8000);
