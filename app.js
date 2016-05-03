2ffef645320cea125656648cc49f2c4f8e232538
amhayslip <amhayslip@gmail.com>
2016-05-03 07:34:25 +0300

    done

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var expressJWT = require('express-jwt');
+
+var auth = expressJWT({secret: 'SECRET'});
+
+var User = require('./UserModel');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -10,7 +15,20 @@ app.use(passport.initialize());
 mongoose.connect('mongodb://localhost/passportjwt');
 
 var LocalStrategy = require('passport-local').Strategy;
-p 
+
+passport.use('login', new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
 app.get('/register', function (req, res) {
   res.sendFile(__dirname + '/register.html');
 });
@@ -19,20 +37,37 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/hello', function (req, res) {
-  res.json({hello: 'world'});
+app.get('/hello', auth, function (req, res) {
+  res.json(req.user);
 });
 
 app.get('/login', function (req, res) {
   res.sendFile(__dirname + '/login.html');
 });
 
-app.post('/register', function(req, res){
+app.post('/register', function(req, res, next){
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
-app.post('/login', function(req, res){
+app.post('/login', function(req, res, next){
+  passport.authenticate('login', function(err, user){
+    if(err){ return next(err); }
 
+    if (user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401);
+    }
+  })(req, res, next);
 });
 
 app.listen(8000);
